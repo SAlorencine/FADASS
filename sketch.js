ca7c2a57d981faf87ce73d49dba3320534653b2d
SAlorencine <92955620+SAlorencine@users.noreply.github.com>
2022-02-24 18:29:42 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+var starImg,bgImg;
+var star, starBody;
+//criar variável para sprite de fada e imgFada
+var fada, fadaIMG, som, fundo
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+function preload()
+{
+    starImg = loadImage("star.png");
+	bgImg = loadImage("starNight.png");
+    //carregar animação de fada 
+    fadaIMG = loadImage("fairyImage1.png","fairyImage2.png");
+    som = loadSound("JoyMusic.mp3")
+    fundo = loadImage("starNight.png")
+}
+
+function setup() {
+    createCanvas(800, 750);
+
+    //escrever código para tocar o som vozFada
+    som.play();
+    //criar sprite de fada e adicionar animação para fada
+    fada = createSprite(100,500);
+    fada.addImage(fadaIMG);
+    fada.scale = 0.2
+
+    star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+    engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+function draw(){
+   background(fundo)
+
+   star.x = starBody.position.x
+   star.y = starBody.position.y
+   
+    if(keyCode == LEFT_ARROW){
+        fada.x = fada.x - 10
+    }
+    if(keyCode == RIGHT_ARROW){
+      fada.x = fada.x + 10
+  }
+
+  if(keyCode == DOWN_ARROW){
+    Matter.Body.setStatic(starBody, false)
+}
+
+  if(star.y > 470 && starBody.position.y > 470 ){
+     Matter.Body.setStatic(starBody, true)
+  }
+   
+
+    drawSprites()
+}
